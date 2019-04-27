cca9391786b3c17d444053e5496b0c9879c71fb0
Phil Howard <phil@gadgetoid.com>
2019-04-27 15:10:44 +0100

    Improved test-pattern with args

--- examples/test-pattern.py
+++ examples/test-pattern.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python
 from drv2605 import DRV2605, PlayWaveform, WaitMillis
-import sys
+import argparse
 import time
 import math
 
 print("""test-pattern.py - Test a DRV2605 built-in haptic pattern
 
-Usage: {} <pattern number>
+""")
 
-""".format(sys.argv[0]))
+parser = argparse.ArgumentParser()
+parser.add_argument('--pattern', type=int, default=1,
+                    help='number of pattern to play')
+parser.add_argument('--repeat', type=int, default=1,
+                    help='number of times to repeat')
+parser.add_argument('--delay', type=int, default=100,
+                    help='delay between repeats')
+parser.add_argument('--calibrate', action='store_true', default=False,
+                    help='perform calibration first')
 
-enable_calibration = True
+args = parser.parse_args()
+
+if args.repeat not in range(1, 5):
+    parser.error("Repeat should be between 1 and 4")
+
+if args.pattern not in range(0, 128):
+    parser.error("Pattern should be between 0 and 127")
+
+if args.delay not in range(0, 128):
+    parser.error("Delay should be between 0 and 127")
 
 drv2605 = DRV2605()
 drv2605.reset()
@@ -18,23 +35,24 @@ drv2605.reset()
 drv2605.set_feedback_mode('LRA')
 drv2605.set_library('LRA')
 
-if enable_calibration:
+if args.calibrate:
     print("Calibrating...")
     drv2605.auto_calibrate()
     time.sleep(0.5)
 
-if len(sys.argv) > 1:
+if args.pattern > 0:
     drv2605.set_mode('Internal Trigger')
-    pattern = int(sys.argv[1])
 
-    print("Playing pattern: {}".format(sys.argv[1]))
+    print("Playing pattern: {}".format(args.pattern))
+
+    sequence = []
+
+    for x in range(args.repeat):
+        sequence.append(PlayWaveform(args.pattern))
+        sequence.append(WaitMillis(args.delay))
 
     drv2605.set_sequence(
-        PlayWaveform(pattern),
-        WaitMillis(100),
-        PlayWaveform(pattern),
-        WaitMillis(100),
-        PlayWaveform(pattern)
+        *sequence
     )
 
     drv2605.go()
