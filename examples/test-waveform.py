0063bb17ffe0f6a7a6decbe23ed7339251525803
Phil Howard <phil@gadgetoid.com>
2019-04-27 15:54:38 +0100

    New example for waveform mode

---
+++ examples/test-waveform.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from drv2605 import DRV2605, PlayWaveform, WaitMillis
+import argparse
+import time
+import math
+
+print("""test-waveform.py - Test DRV2605 real-time waveform mode
+""")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--speed', type=float, default=10,
+                    help='speed of waveform mode')
+parser.add_argument('--intensity', type=int, default=122,
+                    help='intensity of waveform mode')
+parser.add_argument('--calibrate', action='store_true', default=False,
+                    help='perform calibration first')
+
+args = parser.parse_args()
+
+if args.speed < 1 or args.speed > 127:
+    parser.error("Speed should be between 1 and 127")
+
+if args.intensity not in range(1, 123):
+    parser.error("Intensity should be between 1 and 122")
+
+drv2605 = DRV2605()
+drv2605.reset()
+
+drv2605.set_feedback_mode('LRA')
+drv2605.set_library('LRA')
+
+if args.calibrate:
+    print("Calibrating...")
+    drv2605.auto_calibrate()
+    time.sleep(0.5)
+
+print("Playing waveform at speed {} with intensity {}".format(
+      args.speed, args.intensity))
+
+drv2605.set_mode('Real-time Playback')
+drv2605.set_realtime_data_format('Unsigned')
+drv2605.go()
+
+try:
+    while True:
+        d = time.time() * args.speed
+        x = (math.sin(d) + 1) / 2
+        x = int(x * (132 + args.intensity))
+        drv2605.set_realtime_input(x)
+        time.sleep(0.01)
+except KeyboardInterrupt:
+    pass
+finally:
+    drv2605.set_realtime_input(0)
+    drv2605.stop()
